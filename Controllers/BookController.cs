63330d82868e37bf9453f0c43ddd847de051951c
Edison Munoz <edyrecuay@gmail.com>
2022-01-11 21:31:58 -0500

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using BookStore;
+using BookStore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+
+        [HttpPost]
+        public ActionResult<string> AddBooks( [FromBodyAttribute] List<Book> bookList )
+        {
+            foreach (Book book in bookList) {
+
+                var exisitingBook = Program.BookStorage.FirstOrDefault(x => x.Title == book.Title);
+
+                if (exisitingBook != null)
+                {
+                    exisitingBook.stock += book.stock;
+                }
+                else {
+                    Program.BookStorage.Add(book);
+                }
+
+            }
+
+            OkResult ok = new OkResult();
+            return ok;
+        }
+
+
+        [HttpGet]
+        public List<Book> ListBooks() {
+            
+            
+            return Program.BookStorage;
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+       
+        [DataMember(EmitDefaultValue = false)]
+        public string Title { get; set; }
+        [DataMember]
+        public int  stock { get; set; }
+     }
+}
